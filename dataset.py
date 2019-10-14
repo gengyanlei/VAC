27cd52d612eaf901c3f8d466de4739b51f14170f
gengyanlei <1599075165@qq.com>
2019-10-14 09:02:09 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -56,10 +56,10 @@ class CustomDataset(Dataset):
                 ])
 
             self.transform_color = transforms.Compose([
-                    transforms.ColorJitter(brightness=0.4,
-                                           contrast=0.4,
-                                           saturation=0.4,
-                                           hue=0.2),
+                    transforms.ColorJitter(brightness=0.3,
+                                           contrast=0.3,
+                                           saturation=0.2,
+                                           hue=0.1),
                     transforms.ToTensor(),
                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                 ])
